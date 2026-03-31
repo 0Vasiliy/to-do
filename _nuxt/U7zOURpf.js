c8fcbf544aebdcfe4ec441922ee2dfc082026d1a
0Vasiliy <0Vasiliy@users.noreply.github.com>
2026-03-31 19:49:26 +0000

    deploy: 2a94cacf5ee55e8c97cc3c042f1e710e045f001d

---
+++ _nuxt/U7zOURpf.js
@@ -0,0 +1,1 @@
+import{U as n,n as e}from"./BQZSBmrP.js";const r=n(o=>{const t=localStorage.getItem("token");if(o.path==="/login")return t?e("/"):void 0;if(!t)return e("/login")});export{r as default};
